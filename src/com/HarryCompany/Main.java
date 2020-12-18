3e7ad0028af4b4a38fcd60a4de10e790da1835b1
hossein <hr.movahed6@gmail.com>
2020-12-18 12:07:09 +0200

    java

---
+++ src/com/HarryCompany/Main.java
@@ -0,0 +1,15 @@
+package com.HarryCompany;
+
+import java.awt.*;
+import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+      int x = (int)Math.round(Math.random()*1000);
+       NumberFormat c = NumberFormat.getCurrencyInstance();
+          System.out.println(x);
+    }
+}
